72544bb934f73a5f29bb7c2c564590c1addf948c
Frank Schimmel <frank@ueberban.de>
2023-01-17 08:46:13 +0100

    Include price of ton of peanuts in the background.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let requestedData;
+let peanutsData;
 
 let dataArray = [];
 let dataMin;
 let dataMax;
+let peanutsPrice;
 
 // Called once when the program starts just before setup().
 // Use this to load external data, i.e. make your API calls here.
@@ -10,6 +12,7 @@ let dataMax;
 function preload() {
   // Call RKI Covid-19 API
   requestedData = loadJSON('https://api.corona-zahlen.org/germany/history/cases/9');
+  peanutsData = loadTable('https://docs.google.com/spreadsheets/d/e/2PACX-1vQv4HF1VsJRln3h1I8fpkfoTdrprJY2etWfHVhcwFcRJyfcFRLf-YENkxs6XMPFDVzhi11YuQizNbi1/pub?gid=0&single=true&output=csv', 'csv', 'header')
 }
 
 // Called once when the program starts.
@@ -31,6 +34,8 @@ function setup() {
   // Analyse min and max in data
   dataMin = min(dataArray);
   dataMax = max(dataArray);
+
+  peanutsPrice = peanutsData.rows.map(r => Number(r.obj.Preis))
 }
 
 // Called over and over to refresh your visualisation.
@@ -38,6 +43,13 @@ function setup() {
 function draw() {
   // Visualize amount of cases as circles with normalized diameters
   background(0);
+  strokeWeight(0);
+  for (let i = 0; i < peanutsPrice.length; i++) {
+    const grayscale = map(peanutsPrice[i], 0, 2000, 0, 255, true);
+    fill(grayscale);
+    rect(0, map(i, 0, peanutsPrice.length, 0, height), width, height / peanutsPrice.length);
+  }
+  strokeWeight(1);
   for(let i=0; i < dataArray.length; i++){
     const diameter = map(dataArray[i], dataMin, dataMax, 20, 200);
     const x = 100*i;
