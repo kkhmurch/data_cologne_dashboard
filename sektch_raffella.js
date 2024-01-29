a5448497038ab892babb6841cf2cd766c21c8c0d
Malin Lindner <malindner99@t-online.de>
2024-01-29 16:29:33 +0100

    getting all the data

--- sektch_raffella.js
+++ sektch_raffella.js
@@ -1,23 +1,19 @@
 
 function raffaellaPreload() {
-    var myHeaders = new Headers();
-    myHeaders.append("x-access-token", "openuv-1amtywrlrxt94pm-io");
-    myHeaders.append("Content-Type", "application/json");
+    //var myHeaders = new Headers();
+    //myHeaders.append("x-access-token", "openuv-1amtywrlrxt94pm-io");
+    //myHeaders.append("Content-Type", "application/json");
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+    //var requestOptions = {
+    //    method: 'GET',
+    //    headers: myHeaders,
+    //    redirect: 'follow'
+    //};
 
-    fetch("https://api.openuv.io/api/v1/uv?lat=50.93&lng=6.97&alt=100&dt=", requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-
-
-
+    //fetch("https://api.openuv.io/api/v1/uv?lat=50.93&lng=6.97&alt=100&dt=", requestOptions)
+    //    .then(response => response.json())
+    //    .then(result => console.log(result))
+    //    .catch(error => console.log('error', error));
 }
 
 function raffaellaSetup() {
