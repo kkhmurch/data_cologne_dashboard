4e1c863f883132ea9002262dcca4724c63884285
Emma Reeb <emma_laura.reeb@smail.th-koeln.de>
2024-02-01 10:59:07 +0100

    Rhein Setup

--- sektch_emma.js
+++ sektch_emma.js
@@ -1,7 +1,19 @@
+
+let rheinData,rheinPegel;
+
 function emmaPreload() {
+    //Getting the Data on the water level of the Rhein
+    rheinData = loadXML('https://www.stadt-koeln.de/interne-dienste/hochwasser/pegel_ws.php');
+    console.log(rheinData);
+    //httpDo('https://www.stadt-koeln.de/interne-dienste/hochwasser/pegel_ws.php')
+
 }
 
 function emmaSetup() {
+// Extract water level of Rhein from response data.
+    rheinPegel = rheinData.getChild('Pegel');
+    console.log(rheinPegel);
+    print(rheinPegel);
 }
 
 function emmaDraw() {
