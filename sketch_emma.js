7ac6efb38d9fd8a3f18b9e96346f74ee2f507aa2
Emma Reeb <emma_laura.reeb@smail.th-koeln.de>
2024-02-13 12:23:28 +0100

    Rhein is finally working

--- sketch_emma.js
+++ sketch_emma.js
@@ -3,41 +3,31 @@ let rheinData,rheinPegel;
 
 function emmaPreload() {
     //Getting the Data on the water level of the Rhein
-  //  rheinData = loadJSON('');
-   /// console.log(rheinData);
-    //httpDo('https://www.stadt-koeln.de/interne-dienste/hochwasser/pegel_ws.php')
+
 
     let url = 'https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/a6ee8177-107b-47dd-bcfd-30960ccc6e9c.json?includeTimeseries=true&includeCurrentMeasurement=true';
-
     rheinData = loadJSON(url);
 }
 
 function emmaSetup() {
 
 
-
 // Extract water level of Rhein from response data.
-  //  rheinPegel = rheinData.getChild('Pegel');
-   // console.log(rheinPegel);
 
-
-    // Get the content of the 'Pegel' element
-  //  let pegelContent = rheinPegel.getContent();
-   // console.log(pegelContent);
+    rheinPegel = rheinData.timeseries[0].currentMeasurement.value;
+    console.log("Water level:", rheinPegel);
 }
 
-function emmaDraw() {
+    function emmaDraw() {
 
-    //My pixel space
-    // line(768, 0, 768, 1080);
-    // line(768 + 384, 0, 768 + 384, 1080);
-    fill(255);
-    circle(50,50,100);
-    noFill();
+        //My pixel space
+        // line(768, 0, 768, 1080);
+        // line(768 + 384, 0, 768 + 384, 1080);
 
+        stroke(210,0,0);
+        if(rheinPegel) {
+            strokeWeight(rheinPegel / 4);
+            line(768 + 192, 0, 768 + 192, 1080);
+        }
+    }
 
-
-
-
-
-}
