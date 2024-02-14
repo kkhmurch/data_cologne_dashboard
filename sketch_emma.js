5c11c912d1800c7ca99658cec4473f1127a63432
Emma Reeb <emma_laura.reeb@smail.th-koeln.de>
2024-02-14 17:44:54 +0100

    font commit

--- sketch_emma.js
+++ sketch_emma.js
@@ -1,5 +1,7 @@
 
-let rheinData,rheinPegel;
+let rheinData,rheinPegel,font;
+let r = 4;
+
 
 function emmaPreload() {
     //Getting the Data on the water level of the Rhein
@@ -7,6 +9,8 @@ function emmaPreload() {
 
     let url = 'https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/a6ee8177-107b-47dd-bcfd-30960ccc6e9c.json?includeTimeseries=true&includeCurrentMeasurement=true';
     rheinData = loadJSON(url);
+
+    font = loadFont('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
 }
 
 function emmaSetup() {
@@ -16,6 +20,8 @@ function emmaSetup() {
 
     rheinPegel = rheinData.timeseries[0].currentMeasurement.value;
     console.log("Water level:", rheinPegel);
+
+
 }
 
     function emmaDraw() {
@@ -24,10 +30,31 @@ function emmaSetup() {
         // line(768, 0, 768, 1080);
         // line(768 + 384, 0, 768 + 384, 1080);
 
-        stroke(210,0,0);
+        stroke(20,100,140);
         if(rheinPegel) {
-            strokeWeight(rheinPegel / 4);
-            line(768 + 192, 0, 768 + 192, 1080);
+            push();
+            translate(300, 200);
+            let total = floor(height / (r * 2));
+            noFill();
+            strokeWeight(rheinPegel -400);
+
+            beginShape();
+            for (let i = 0; i < total + 1; i++) {
+                let angle = map(i, 100, total, 0, TWO_PI);
+                let x = map(sin(angle), -1, 1, 600, 660);
+                let y = map(i, 0, total + 1, -300, 1080);
+                vertex(x, y);
+            }
+            endShape();
+
+            pop();
+
+            fill(255);
+            textFont(font);
+            textSize(100);
+            noStroke();
+            text(rheinPegel + ' hello world', 1500, 1000);
+
         }
     }
 
